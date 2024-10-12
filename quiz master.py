2144f9b0f421b08fdbd7b199fc54aca58510bd93
dara-0912 <darateetee09@gmail.com>
2024-10-12 13:33:18 +0100

    Add files via upload

---
+++ quiz master.py
@@ -0,0 +1,16 @@
+import pgzrun 
+title =  "Quiz master"
+WIDTH = 870
+HEIGHT = 650
+marquee_box = Rect(0,0,880,80)
+question_box = Rect(0,0,650,150)
+timer_box = Rect(0,0,150,150)
+answer_box1 = Rect(0,0,300,150)
+answer_box2 = Rect(0,0,300,150)
+answer_box3 = Rect(0,0,300,150)
+answer_box4 = Rect(0,0,300,150)
+skip_box = Rect(0,0,150,330)
+score = 0
+timer_left = 10
+question_file_name = "question.txt"
+
